70b536f9e1a243908b86233b14950d2a91ea6e35
bernardcli <bernardcli@37f6f168-522c-0410-92d0-dd5a70235d87>
2007-03-23 06:53:34 +0000

    Initial import of qaoed v0.77

---
+++ Makefile
@@ -0,0 +1,26 @@
+CC=gcc
+CFLAGS= -Wall -g -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64
+LIBS = -lpthread
+
+all:    qaoed
+
+OBJS  = devices.o network.o main.o logging.o acl.o parseconf.o
+RCFGOBJS = rcfg/readconf.o
+ARCHOBJS = arch/bsd_net.o arch/linux_net.o
+INCLUDES = include/acl.h include/eth.h include/aoe.h include/qaoed.h \
+	   include/hdreg.h include/logging.h include/byteorder.h
+
+$(OBJS): $(INCLUDES)
+
+clean:
+	rm -f *.o *~ core qaoed include/*~ rcfg/*~ rcfg/*.o arch/*.o \
+	arch/*~ man/*~ examples/*~
+
+qaoed:	$(OBJS) $(RCFGOBJS) $(ARCHOBJS) $(INCLUDES)
+	make -C arch
+	make -C rcfg
+	gcc $(CFLAGS) -o qaoed $(OBJS) $(RCFGOBJS) $(ARCHOBJS) $(LIBS)
+	strip qaoed 
+
+
+
---
+++ include/qaoed.h
@@ -0,0 +1,136 @@
+#include <pthread.h>
+#include "aoe.h"
+#include "../rcfg/rcfg.h"
+
+#define CFGFILE "/etc/qaoed.conf"
+
+#define  ETH_P_AOE 0x88a2
+
+/* The different types of work requests */
+#define WORKTYPE_PACKET 1  /* Work request contains a packet */
+
+
+/* Status of device/interface threads */
+#define PTSTATUS_IDLE 0
+#define PTSTATUS_RUNNING 1
+
+struct threadargs{
+  struct qconfig *conf;
+  struct ifst *ifentry;
+};
+
+struct pkt 
+{
+  void *raw; /* The real start of the packet, used for free and malloc */
+  int refcount; /* Used to indicate if this buffer is still needed */
+  pthread_mutex_t pktlock; /* Lock for this buffer, only used to atomic
+                            * access to the recounter  */
+};
+
+struct workentry
+{
+   unsigned char type;
+   struct pkt *rawbuf;
+   struct aoe_hdr *aoepkt;
+   int packet_len;
+   struct ifst *ifentry; /* Interface that the packet came in on */
+   pthread_mutex_t worklock; /* Lock for this workentry */
+   int refcount; /* Refcount is used to indicate if this 
+		  * workentry can be destroyed or not */
+};
+
+struct queueentry 
+{
+   struct workentry *work;
+   struct queueentry *next;
+};
+
+
+/* Used to describe an interface used for communication */
+struct ifst
+{
+   char *ifname;           /* Name of interface */
+   unsigned char status;   /* Status of interface thread */
+   pthread_t threadID;     /* Thread id if thread handling this interface */
+   unsigned short ifindex; /* Interface index number (if any) */
+   int sock;               /* Outgoing socket for reply */
+   char *hwaddr;           /* src-address to use when sending */
+   int mtu;                /* the Maximum Transfer Unit (MTU) of this int... */
+   int buffsize;           /* Buffer size to use when reading from socket */
+   struct logging *log;    /* Pointer to the logging target for this int... */
+   int log_level;          /* The log-level of this interface */
+   struct ifst *next;
+};
+
+
+
+/* List of devices currently avaiable, this list can be used both 
+ * to find the right queue for a device and during startup to 
+ * start the worker threads. */
+struct aoedev 
+{
+   char *devicename;       /* Point to file/device name */
+   int fd;                 /* file descriptor of open file */
+   FILE *fp;               /* File pointer to open file */
+   off_t size;             /* size in 512byte blocks */
+   unsigned short shelf;   /* 16 bit */
+   unsigned char slot;     /*  8 bit */
+   struct ifst *interface; /* interface */
+   unsigned char status;   /* Status of device thread */
+   pthread_t threadID;     /* Thread id if thread handling this device */
+   int writecache;         /* Writecache on or off */
+   int broadcast;          /* Broadcast on or off */
+   
+   void *wacl;             /* Access list used for write operations    */
+   void *racl;             /* Access list used for read operations     */
+   void *cfgsetacl;        /* Access list used for cfg set             */
+   void *cfgracl;          /* Access list used for cfg read / discover */
+   
+   pthread_cond_t qcv;        /* Tell thread something is in the queue */
+   pthread_mutex_t queuelock; /* Lock used to access this queue */
+   struct queueentry *q;           /* The first for this device */
+   struct queueentry *qstart;      /* The first entry  of this queue */
+   struct queueentry *qend;        /* The last entry  of this queue  */
+   struct logging *log;            /* Logging */
+   int log_level;             /* Loglevel for this device */
+   int cfg_len;               /* The cfg-data for this device */
+   char cfg[1024];            /* The cfg-data for this device */
+   struct aoedev *next;       /* Pointer to the next device */
+};
+
+
+struct qconfig 
+{
+   struct logging *log;        /* Logtarget to use by default */
+   int log_level;              /* Loglevel for the qaoe daemon */
+   int syslog_facility;        
+   int syslog_level;
+   
+   struct ifst   *intlist;      /* list of interfaces */
+   struct aoedev *devdefault;  /* default values for devices */
+   struct aoedev *devices;     /* Linked list with devices */
+   struct aclhdr *acllist;     /* Linked list with access-lists */
+   struct logging *loglist;    /* linked list with logging targets */
+   
+};
+
+struct qconfig *qaoed_loadconfig();
+int qaoed_devices(struct qconfig *conf);
+int qaoed_network(struct qconfig *conf);
+void qaoed_workdestroy(struct workentry *work);
+int qaoed_xmit(struct workentry *work, void *pkt, int pkt_len);
+  
+struct aoedev * findDEV(struct aoedev *devlist, struct cfg *c, 
+			struct qconfig *conf);
+struct aoedev * findDFLT(struct aoedev *devlist, struct cfg *c,
+		         struct qconfig *conf);
+
+int qaoed_opensock(struct ifst *ifentry);
+int getifhwaddr(struct ifst *ifentry);
+int getifmtu(struct ifst *ifentry);
+struct ifst *referenceint(char *name, struct qconfig *conf);
+void destroyqcfg(struct qconfig *conf);
+int qaoed_listener(struct threadargs *args);
+int qaoed_pktdestroy(struct pkt *packet);
+void processpacket(struct qconfig *conf, struct ifst *ifentry, 
+		   struct pkt *Packet, struct aoe_hdr *aoepkt, int len);
